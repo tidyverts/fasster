ac227d9e974364be37e02997af650b6e6bdabf6d
Unknown <mitch.ohara-wild@monash.edu>
2017-08-21 15:30:22 +1000

    Model fitting algorithm update

--- R/fasster.r
+++ R/fasster.r
@@ -5,7 +5,6 @@
 #' @export
 dplyr::`%>%`
 
-
 formula_parse_infix <- function(.formula, infix=NULL){
   fn_name <- deparse(.formula[[1]])
   if(fn_name %in% infix){
@@ -28,12 +27,12 @@ formula_parse_groups <- function(.formula){
   mt <- terms(.formula, specials = ".infix.G", allowDotAsName=TRUE)
   groupIdx <- attr(mt, "specials")[[".infix.G"]]
 
-  sub_mt <- if(is.null(groupIdx)) delete.response(mt)
-            else delete.response(mt)[-groupIdx]
+  sub_mt <- if(is.null(groupIdx)) mt
+            else mt[-groupIdx]
 
   ## Add current layer's formula
   if(length(attr(sub_mt, "variables")) > 1){
-    parse[["formula"]] <- formula(sub_mt)
+    parse[["model"]] <- formula(sub_mt)
   }
 
   ## Explore more groups
@@ -46,4 +45,118 @@ formula_parse_groups <- function(.formula){
   }
 
   parse
+}
+
+spread_groups <- function(object){
+  out <- NULL
+  for(group in levels(object)){
+    out <- cbind(out, as.numeric(object == group))
+  }
+  colnames(out) <- levels(object)
+  out
+}
+
+build_FASSTER_group <- function(model_struct, data, groups=NULL){
+  if(!is.null(groups)){
+    ## Grouped model
+    groupVar <- as.formula(paste("~", paste(groups, collapse =" + ")))
+    terms_groups <- terms(groupVar, data=data)
+    attr(terms_groups, "intercept") <- 0
+    groupData <- model.matrix(terms_groups, data) %>% as.data.frame %>% interaction
+
+    groupX <- spread_groups(groupData)
+  }
+
+  model_struct[["model"]] <- build_FASSTER(model_struct[["model"]], data, X = NULL)
+  model_struct
+}
+
+#' @importFrom rlang eval_tidy
+build_FASSTER <- function(formula, data, X = NULL){
+  dlmTerms <- list()
+
+  ## Deparse model specification
+  triggerwords <- c("constant", "intercept", "slope", "trend")
+  specials <- c("poly", "trig", "seas")#, "fourier", "seasonality", "seasonal")
+
+  mt <- terms(formula, data = data, specials = specials)
+  attr(mt, "intercept") <- 0
+
+  extractSpecialArgs <- function(x){
+    if(is.null(x)) NULL
+    else eval_tidy(as.list(attr(mt, "variables")[[1L + x]])[-1])
+  }
+  specialTerms <- attr(mt, "specials") %>%
+    as.list %>%
+    map(~ .x %>% lapply(extractSpecialArgs))
+
+  browser()
+
+  specialIdx <- unlist(attr(mt, "specials"))
+
+  ## Set up xreg
+  if(length(attr(mt, "term.labels")) > length(specialIdx)){
+    if(!is.null(specialIdx)){
+      mt <- mt[-c(specialIdx-1)]
+    }
+    xreg <- model.matrix(mt, data)
+    dlmTerms <- append(dlmTerms, dlmModReg(xreg)) ## TODO: Multiply model matrix by group indicator for grouping
+  }
+
+}
+
+#' Fast Additive Seasonal Switching with Trend and Exogenous Regressors
+#'
+#' @param data A data.frame
+#' @param model The fitted model
+#' @param grouping The column in data for which the model switches.
+#' @param lambda Box-Cox transformation parameter. Ignored if NULL. Otherwise, data transformed before model is estimated. When lambda is specified, additive.only is set to TRUE.
+#' @param approx If `TRUE`, the fitted parameters for the model be approximated using stl decomposition. Otherwise, the parameters are optimised via the MLE.
+#' @param ...
+#'
+#' @return Returns a fitted FASSTER model
+#' @export
+#'
+#' @examples
+#'
+#' @importFrom lazyeval f_capture
+#' @importFrom dplyr select
+#' @importFrom rlang eval_tidy f_rhs
+#'
+fasster <- function(data, model = y ~ intercept + trig(24) + trig(7*24) + xreg, groupVar, lambda=NULL, approx=TRUE, s.window = 50, ...){
+  series <- all.vars(model)[1]
+  y <- data[,series]
+  if(!is.null(lambda))
+    y <- BoxCox(y, lambda)
+
+  # Parse formula into structure
+  infix_model <- formula_parse_infix(model, "%G%")
+  model_struct <- formula_parse_groups(infix_model)
+
+  # out <- build_FASSTER_group(model_struct, data)
+  #
+  # stlFreq <- specialTerms[-1] %>% # Remove poly special
+  #   map(~ .x %>% map_dbl(~ .x[[1]])) %>% # Extract first argument of seasonal specials
+  #   unlist()
+  #
+  # initialOptim <- fasster_stl(y, groupData, s.window=s.window, stlFreq)
+
+
+  ## Build modelFn
+  # For each model group (including no-group), build a seperate dlm structure
+  # - GroupVar (with specific model, compute number of required params)
+  # -- GroupLevelMod (attach groupX)
+  # -- GroupLevelMod (attach groupX)
+  # - NoGroup (with specific model, compute number of required params)
+  # -- noGroupMod
+
+  if(!approx){
+    # Setup function
+    # Run dlmMLE (perhaps after fasster_stl)
+    stop("Not yet implemented")
+    getOptim <- dlmMLE(data[,series], 0, fn)
+  }
+
+  ## Fit model
+  fit <- dlmFilter(y, out)
 }
