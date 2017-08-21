530fb5fc7e6faf0e66f8091c8fd7476271c7635a
Unknown <mitch.ohara-wild@monash.edu>
2017-08-21 19:22:14 +1000

    Reduced imports

--- R/fasster.r
+++ R/fasster.r
@@ -11,7 +11,7 @@ formula_parse_infix <- function(.formula, infix=NULL){
     .formula[[1]] <- as.name(paste(".infix", substr(fn_name, 2, nchar(fn_name) - 1), sep="."))
   }
   for(pos in 2:length(.formula)){
-    if(is_call(.formula[[pos]])){
+    if(is.call(.formula[[pos]])){
       .formula[[pos]] <- formula_parse_infix(.formula[[pos]], infix)
     }
   }
