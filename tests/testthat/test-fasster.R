aec9af39420a4b2e453e7124e33561f4c4ae5b89
Unknown <mitch.ohara-wild@monash.edu>
2017-09-04 15:55:00 +1000

    Updated tests

--- tests/testthat/test-fasster.R
+++ tests/testthat/test-fasster.R
@@ -1,14 +1,18 @@
 library(fpp2)
 context("fasster")
 
-test_that("Speed test", {
-  fit1 <- elecdemand %>% fasster(Demand ~  trig(24) + Temperature, heuristic = FALSE)
-  fit1_optim <- elecdemand %>% fasster(Demand ~  trig(24) + Temperature)
+test_that("Test grouping", {
+  fit1 <- elecdemand %>%
+    fasster(Demand ~  trig(24) + Temperature)
+  fit2 <- elecdemand %>%
+    fasster(Demand ~ WorkDay %G% (trig(24) + Temperature))
 
-
-  fit2 <- elecdemand %>% fasster(Demand ~  trig(24) + Temperature, heuristic = TRUE)
-
-
-  fit2 <- elecdemand %>% fasster(Demand ~  trig(24) + Temperature, heuristic = TRUE)
-
+  USAccDeaths %>%
+    data.frame() %>%
+    fasster(`.` ~ poly(1) + trig(12)) %>%
+    forecast.fasster() %>%
+    autoplot()
+  ## 902177.7
+  ## 239660.5
+  ## 266374
 })
