7c4dc2cbb2d596f5587ceb3600c6c8e26bd4c1ba
Unknown <mitch.ohara-wild@monash.edu>
2017-10-09 13:17:50 +1100

    Fixed commenting of unused heuristics

--- R/heuristic.r
+++ R/heuristic.r
@@ -1,40 +1,39 @@
-#' #' @importFrom forecast seasonal trendcycle remainder
-#' var_stl_decomp <- function(stlobj) {
-#'   list(
-#'     seasonal = var(diff(stlobj[, "seasonal"], frequency(stlobj))),
-#'     trendcycle = var(diff(stlobj[, "trend"])),
-#'     remainder = var(stlobj[, "remainder"]),
-#'     stlobj = stlobj
-#'   )
-#' }
-#'
-#' #' @export
-#' #' @import purrr
-#' #' @importFrom magrittr extract2
-#' fasster_stl <- function(response, groupData, s.window, stlFreqs) {
-#'
-#'   ## Rough structure
-#'   # - Take in object, determine grouping and required variables
-#'   # - Split by each group and perfrom stl decomposition on the response, with frequency according to each seasonal term in the switching model
-#'
-#'   ## Estimate optimal parameters specific to model groupings
-#'   browser()
-#'   out <- response %>%
-#'     split(groupData) %>%
-#'     map(~ list(.x) %>%
-#'       map2(stlFreqs, ~ .x %>%
-#'         ts(frequency = .y) %>%
-#'         stl(s.window = s.window) %>%
-#'         extract2("time.series") %>%
-#'         var_stl_decomp()
-#'       )
-#'     )
-#'   # out$`_total` <- out %>%
-#'   #   map(~ .x %>%
-#'   #         extract2("stlobj")) %>%
-#'   #   invoke(rbind, .) %>%
-#'   #   var_stl_decomp()
-#' }
+# #' @importFrom forecast seasonal trendcycle remainder
+# var_stl_decomp <- function(stlobj) {
+#  list(
+#    seasonal = var(diff(stlobj[, "seasonal"], frequency(stlobj))),
+#    trendcycle = var(diff(stlobj[, "trend"])),
+#    remainder = var(stlobj[, "remainder"]),
+#    stlobj = stlobj
+#  )
+# }
+
+# #' @import purrr
+# #' @importFrom magrittr extract2
+# fasster_stl <- function(response, groupData, s.window, stlFreqs) {
+#
+#  ## Rough structure
+#  # - Take in object, determine grouping and required variables
+#  # - Split by each group and perfrom stl decomposition on the response, with frequency according to each seasonal term in the switching model
+#
+#  ## Estimate optimal parameters specific to model groupings
+#  browser()
+#  out <- response %>%
+#    split(groupData) %>%
+#    map(~ list(.x) %>%
+#      map2(stlFreqs, ~ .x %>%
+#        ts(frequency = .y) %>%
+#        stl(s.window = s.window) %>%
+#        extract2("time.series") %>%
+#        var_stl_decomp()
+#      )
+#    )
+#  # out$`_total` <- out %>%
+#  #   map(~ .x %>%
+#  #         extract2("stlobj")) %>%
+#  #   invoke(rbind, .) %>%
+#  #   var_stl_decomp()
+# }
 
 
 dlm_lmHeuristic <- function(y, dlmModel){
