e55827198831410d03416f8784372ba52220b113
Unknown <mitch.ohara-wild@monash.edu>
2017-10-09 13:18:04 +1100

    Fixed package help file

--- R/fasster.r
+++ R/fasster.r
@@ -1,5 +1,18 @@
-#' @keywords internal
-"_PACKAGE"
+#' Fast Additive Switching of Seasonality, Trend and Exogenous Regressors
+#'
+#' Implementation of the FASSTER model for forecasting time series
+#' with multiple seasonalities using switching states.
+#'
+#' \tabular{ll}{ Package: \tab fasster\cr Type: \tab Package\cr License: \tab
+#' GPL3\cr LazyLoad: \tab yes\cr }
+#'
+#' @docType package
+#' @name fasster-package
+#' @author Mitchell O'Hara-Wild
+#'
+#' Maintainer: mail@@mitchelloharawild.com
+#' @keywords package
+NULL
 
 #' @importFrom dplyr %>%
 #' @export
