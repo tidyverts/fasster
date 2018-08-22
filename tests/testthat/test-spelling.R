228e25fa5033dc9b3b001c698085f9faa6f8faab
Mitchell <mitch.ohara-wild@monash.edu>
2018-08-22 13:24:49 +1000

    Updated spelling check

--- tests/testthat/test-spelling.R
+++ tests/testthat/test-spelling.R
@@ -1,8 +1,14 @@
 context("test-spelling.R")
 
 test_that("package spell check", {
-  skip_on_travis()
-  badspell <- spelling::spell_check_package("../../")
-  expect_equal(NROW(badspell), 0, 
-               info = capture.output(print(badspell)))
+  skip_on_cran()
+
+  # Determine package source path by finding README.md
+  package_dir <- dirname(list.files("../../", pattern = "README.md", recursive = TRUE, full.names = TRUE))
+  skip_if(length(package_dir) != 1)
+  badspell <- spelling::spell_check_package(
+    package_dir
+  )
+
+  expect_equal(NROW(badspell), 0, info = capture.output(print(badspell)))
 })
