94a039d95e96831cefe47b02a3b8bf2045b6fb33
Mitchell <mitch.ohara-wild@monash.edu>
2018-08-22 16:12:51 +1000

    Added modelling to setup

--- tests/testthat/setup-tests.R
+++ tests/testthat/setup-tests.R
@@ -2,3 +2,6 @@ context("setup-tests.R")
 
 UKLungDeaths <- as_tsibble(cbind(mdeaths, fdeaths), gather = FALSE)
 USAccDeaths <- as_tsibble(USAccDeaths)
+
+USAccDeaths_fit <- USAccDeaths %>%
+  fasster(value ~ poly(1) + seas(12))
