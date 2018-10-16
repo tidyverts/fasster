d5ce822218d307de13c3d8f9c68f3c085a1bb9b8
Mitchell <mitch.ohara-wild@monash.edu>
2018-10-16 11:31:09 +1100

    Updated components method to new object structure

--- R/components.R
+++ R/components.R
@@ -4,13 +4,13 @@ components.FASSTER <- function(object, ...) {
   groups <- object$dlm$FF %>%
     colnames() %>%
     factor
+
   states <- levels(groups) %>%
     map_dfc(~ as.numeric(groups == .x)) %>%
     set_names(levels(groups)) %>%
     as.matrix %>%
     t
 
-  data.frame(index = object$index) %>%
-    cbind((states * object$dlm$FF[rep(1,NROW(states)),]) %*% t(object$states) %>% t %>% as_tibble) %>%
-    as_tsibble(index=index)
+  combined <- as_tibble(object$states %*% t(states * object$dlm$FF[rep(1,NROW(states)),]))
+  transmute(object$est, !!!combined)
 }
