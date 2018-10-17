c48f57cf74bee11666346817fbdfb9d7b9dd914c
Mitchell <mitch.ohara-wild@monash.edu>
2018-10-17 12:23:54 +1100

    Updated forecast tests for new fablelite structure

--- tests/testthat/test-forecast.R
+++ tests/testthat/test-forecast.R
@@ -2,18 +2,15 @@ context("test-forecast.R")
 
 test_that("Test fasster forecasting", {
   fc1 <- forecast(USAccDeaths_fit)
-  fc1_sum <- summary(fc1)
-  expect_equal(NROW(fc1_sum), 24)
+  expect_equal(NROW(fc1), 24)
 
-  fc2 <- forecast(USAccDeaths_fit, h = 3)
-  fc2_sum <- summary(fc2)
-  expect_equal(NROW(fc2_sum), 3)
+  fc2 <- forecast(USAccDeaths_fit, 3)
+  expect_equal(NROW(fc2), 3)
 
   future_idx <- tsibble::yearmonth(as.Date(USAccDeaths$index) + months(NROW(USAccDeaths)))
   fc3 <- forecast(USAccDeaths_fit, newdata = tsibble::as_tsibble(list(index = future_idx), index = index))
-  fc3_sum <- summary(fc3)
-  expect_equal(NROW(fc3_sum), NROW(USAccDeaths))
+  expect_equal(NROW(fc3), NROW(USAccDeaths))
 
-  expect_equal(fc1_sum$mean[1:3], fc2_sum$mean[1:3])
-  expect_equal(fc1_sum$mean[1:3], fc3_sum$mean[1:3])
+  expect_equal(fc1$mean[1:3], fc2$mean[1:3])
+  expect_equal(fc1$mean[1:3], fc3$mean[1:3])
 })
