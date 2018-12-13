ea4e13003a3c602a093fb046fa8645e051ef5eb7
Mitchell <mitch.ohara-wild@monash.edu>
2018-12-13 16:59:27 +1100

    Moved specials to be available when needed

--- R/model.R
+++ R/model.R
@@ -46,8 +46,96 @@ train_fasster <- function(.data, formula, specials, include = NULL){
          est = .data %>% mutate(.fitted = filtered$f, .resid = resid),
          states = filtered$a, formula = set_env(formula, new_environment())),
     class = "FASSTER")
-
 }
+
+.specials <- new_specials_env(
+  `%S%` = function(group, expr){
+    group_expr <- enexpr(group)
+    lhs <- factor(group)
+    groups <- levels(lhs) %>% map(~ as.numeric(lhs == .x)) %>% set_names(levels(lhs))
+
+    rhs <- parse_model_rhs(enexpr(expr), data = .data, specials = .specials)$specials %>%
+      unlist(recursive = FALSE) %>%
+      reduce(`+`)
+
+    groups %>%
+      imap(function(X, groupVal){
+        if(is.null(rhs$JFF)){
+          rhs$JFF <- rhs$FF
+          rhs$X <- matrix(X, ncol = 1)
+        }
+        else{
+          rhs$X <- rhs$X * X
+          if(any(new_X_pos <- rhs$FF!=0 & rhs$JFF==0)){
+            new_X_col <- NCOL(rhs$X) + 1
+            rhs$JFF[new_X_pos] <- new_X_col
+            rhs$X <- cbind(rhs$X, X)
+          }
+        }
+        colnames(rhs$X) <- paste0(expr_text(group_expr), "_", groupVal, "/", colnames(rhs$X))
+        colnames(rhs$FF) <- paste0(expr_text(group_expr), "_", groupVal, "/", colnames(rhs$FF))
+        rhs
+      }) %>%
+      reduce(`+`)
+  },
+  `(` = function(expr){
+    parse_model_rhs(enexpr(expr), data = .data, specials = .specials)$specials %>%
+      unlist(recursive = FALSE) %>%
+      reduce(`+`)
+  },
+  poly = function(...){
+    cl <- match.call()
+    out <- dlmModPoly(...)
+    colnames(out$FF) <- rep(deparse(cl), NCOL(out$FF))
+    out
+  },
+  seas = function(...){
+    cl <- match.call()
+    out <- dlmModSeas(...)
+    colnames(out$FF) <- rep(deparse(cl), NCOL(out$FF))
+    out
+  },
+  seasonal = function(...){
+    cl <- match.call()
+    out <- dlmModSeas(...)
+    colnames(out$FF) <- rep(deparse(cl), NCOL(out$FF))
+    out
+  },
+  trig = function(...){
+    cl <- match.call()
+    out <- dlmModTrig(...)
+    colnames(out$FF) <- rep(deparse(cl), NCOL(out$FF))
+    out
+  },
+  fourier = function(...){
+    cl <- match.call()
+    out <- dlmModTrig(...)
+    colnames(out$FF) <- rep(deparse(cl), NCOL(out$FF))
+    out
+  },
+  ARMA = function(...){
+    cl <- match.call()
+    out <- dlmModARMA(...)
+    colnames(out$FF) <- rep(deparse(cl), NCOL(out$FF))
+    out
+  },
+  custom = function(...){
+    cl <- match.call()
+    out <- dlm(...)
+    colnames(out$FF) <- rep(deparse(cl), NCOL(out$FF))
+    out
+  },
+  xreg = function(...){
+    model_formula <- new_formula(
+      lhs = NULL,
+      rhs = reduce(c(0, enexprs(...)), ~ call2("+", .x, .y))
+    )
+    mm <- eval_tidy(model.matrix(model_formula), data = .data)
+    out <- dlmModReg(mm, addInt = FALSE)
+    colnames(out$FF) <- colnames(mm)
+    out
+  }
+)
 
 #' Fast Additive Switching of Seasonality, Trend and Exogenous Regressors
 #'
