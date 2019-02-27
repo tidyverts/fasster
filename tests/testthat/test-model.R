11f830e139eb47c5b946a4c91f1f211fa1e4f942
Mitchell <mitch.ohara-wild@monash.edu>
2019-02-27 12:37:11 +1100

    Updated fasster tests

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -3,9 +3,9 @@ context("test-model.R")
 test_that("Test fasster modelling", {
   expect_s3_class(USAccDeaths_fit, "mdl_df")
   expect_output(print(USAccDeaths_fit), "FASSTER", fixed = TRUE)
-  expect_output(summary(USAccDeaths_fit), "State noise variances (W):", fixed = TRUE)
-  expect_output(summary(USAccDeaths_fit), "Observation noise variance (V):", fixed = TRUE)
-  expect_output(report(USAccDeaths_fit), "FASSTER Model", fixed = TRUE)
+  expect_output(report(USAccDeaths_fit), "State noise variances (W):", fixed = TRUE)
+  expect_output(report(USAccDeaths_fit), "Observation noise variance (V):", fixed = TRUE)
+  expect_output(report(USAccDeaths_fit), "seas(12)", fixed = TRUE)
 
   expect_equivalent(USAccDeaths_fit[["fasster"]][[1]]$fit$dlm$FF, c(1,1, rep(0, 10)))
   expect_equivalent(USAccDeaths_fit[["fasster"]][[1]]$fit$dlm$V, 8476.284)
