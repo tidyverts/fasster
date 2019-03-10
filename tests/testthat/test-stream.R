37041f85d38b415f6cea3200d82e4e2af33e4fbe
Mitchell <mitch.ohara-wild@monash.edu>
2019-03-10 18:22:15 +1100

    Fixed test dataset

--- tests/testthat/test-stream.R
+++ tests/testthat/test-stream.R
@@ -1,12 +1,13 @@
 context("test-stream.R")
 
 test_that("streaming works", {
-  fit_p1 <- tsibbledata::UKLungDeaths %>%
+  UKLungDeaths <- cbind(mdeaths, fdeaths) %>% as_tsibble(gather = FALSE)
+  fit_p1 <- UKLungDeaths %>%
     head(-12) %>%
     model(FASSTER(mdeaths ~ fdeaths + poly(1) + trig(12)))
 
   fit_p2 <- fit_p1 %>%
-    stream(tsibbledata::UKLungDeaths %>% tail(12))
+    stream(UKLungDeaths %>% tail(12))
 
   expect_equal(fitted(fit_p1), head(fitted(fit_p2), -12))
 })
