63f53d7aae5bc88adb53a30752a6a55a4022233c
Mitchell <mitch.ohara-wild@monash.edu>
2019-09-19 12:17:23 +1000

    Updated `new_data` unit test for tsibble changes

--- tests/testthat/test-forecast.R
+++ tests/testthat/test-forecast.R
@@ -7,8 +7,7 @@ test_that("Test fasster forecasting", {
   fc2 <- forecast(USAccDeaths_fit, h = 3)
   expect_equal(NROW(fc2), 3)
 
-  future_idx <- tsibble::yearmonth(as.Date(USAccDeaths$index) + months(NROW(USAccDeaths)))
-  fc3 <- forecast(USAccDeaths_fit, new_data = tsibble::as_tsibble(list(index = future_idx), index = index))
+  fc3 <- forecast(USAccDeaths_fit, new_data = new_data(USAccDeaths, NROW(USAccDeaths)))
   expect_equal(NROW(fc3), NROW(USAccDeaths))
 
   expect_equal(fc1$value[1:3], fc2$value[1:3])
